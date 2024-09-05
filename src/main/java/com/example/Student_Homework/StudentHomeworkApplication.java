8365d825b5b907a6ebbce852298ff0ede96c2cee
Raneem <Raneem.qari@gmail.com>
2024-09-05 17:02:07 +0300

    init repo

---
+++ src/main/java/com/example/Student_Homework/StudentHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.Student_Homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Student_Homework/StudentHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Student_Homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
